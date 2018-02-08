5111ec8d158faca9c90218f27b15650ff69a8d92
Islam Dudaev <islamwolf23@gmail.com>
2018-02-08 23:06:39 +0000

    js execises

--- main.js
+++ main.js
@@ -5,4 +5,79 @@
       node class-exercises.js
 */
 
-console.log('Hello World');
+//console.log('Hello World. I just started learning JavaScript!');
+//console.log('My name is Islam Dudaev');
+//console.log('I like coding..');
+//console.log(28);
+
+/*
+console.log(typeof 'Hello world!');
+console.log(typeof 6);
+console.log(typeof false);
+*/
+
+
+//var city = 'Glasgow';
+//console.log(city);
+
+
+/*
+var courseName = 'Code Your Future';
+var courseDuration = 6;
+var isHard = false;
+
+console.log(typeof courseName);
+console.log(typeof courseDuration);
+console.log(typeof isHard);
+*/
+
+
+
+/*
+var isHappy = true;
+
+if (isHappy === true) {
+  // if I am happy, print "Hello world!"
+  console.log('Hello world!');
+} else {
+  // if I am sad, print a frowny face
+  console.log(':(');
+}
+*/
+
+
+
+
+/*
+var number = 6;
+
+if (number % 2 === 0) {
+  // the number is even
+  console.log('even');
+} else {
+  // the number is odd
+  console.log('odd');
+}
+*/
+
+
+
+
+var number = 3;
+
+if (number > 0 && number%2 === 0) {
+  console.log('positive even');
+} else if(number > 0 && number%2 === 1) {
+  console.log('positive odd');
+} else if(number < 0 && number%2 === 0){
+  console.log('negative even');
+} else {
+  console.log('negative odd');
+}
+
+
+
+
+
+
+
