4297d7bd31489aa1df15b66b902f798c488fd905
Islam Dudaev <islamwolf23@gmail.com>
2018-03-10 12:49:57 +0000

    Complete challenge02.js

--- homework/challenge02.js
+++ homework/challenge02.js
@@ -1,6 +1,8 @@
 /* Challenge 2: FizzBuzz
 
-Write a for loop that prints the numbers from 1 to 100. But for multiples of 3 print “Fizz” instead of the number and for the multiples of 5 print “Buzz”. For numbers which are multiples of both 3 and 5 print “FizzBuzz”.
+Write a for loop that prints the numbers from 1 to 100. 
+But for multiples of 3 print “Fizz” instead of the number and for the multiples of 5 print “Buzz”. 
+For numbers which are multiples of both 3 and 5 print “FizzBuzz”.
 
 Counting to 15 should look like this:
 
@@ -15,9 +17,21 @@ Counting to 15 should look like this:
 'Fizz'
 'Buzz'
 11
-'FizzBuzz'
+'Fizz'
 13
 14
 'FizzBuzz'
 
 */
+
+for(var i=1; i<=100; i++){
+    if((i%3 === 0) && (i%5 === 0)){
+        console.log("FizzBuzz");
+    } else if(i%3 === 0){
+        console.log("Fizz");
+    } else if(i%5 === 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
